b460b60461fcfc0b6d046946b546e90d4fdffec5
Emily Finchum-Mason <47533884+eafinchum@users.noreply.github.com>
2019-06-26 17:46:22 -0700

    Add code to clean spatially-joined permit data

---
+++ tools/clean_permitdemo_data.R
@@ -0,0 +1,7 @@
+library(dplyr)
+
+permits_demo <- read.csv("permits_demographicdata.csv",
+                           header = T,
+                           sep = ",")
+permits_demo <- permits_demo %>%
+  filter()
