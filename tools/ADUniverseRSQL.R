71aed42b742ce68238333f73be172afb9a2dc583
Adrian T <43564152+adrianmt18@users.noreply.github.com>
2019-06-28 17:08:04 -0700

    Create ADUniverseRSQL.R
    Upload an R file usable for running SQL queries on an SQLite3 db file

---
+++ tools/ADUniverseRSQL.R
@@ -0,0 +1,35 @@
+library(sqldf)
+library(DBI)
+library(RSQLite)
+
+select <- function(table, cols = '*', cn = con) {
+  #table: database table to select table from
+  #cols (optional): string of columns to select from table. Default is * (all columns)
+  #cn = sqlite3.Connection object
+  
+  #Example:
+  #con <- dbConnect(SQLite(), dbname = "mydata.db")
+  #select("tablename")
+  #-OR-
+  #select("tablename", "col1, col2, col3")
+  selString = sprintf("select %s from %s", cols, table)
+  dbGetQuery(con, selString)
+}
+
+# Ensure there is no existing db connection
+if (exists("db")) dbDisconnect(con)
+
+# connect to the sqlite file
+con <- dbConnect(SQLite(),  dbname = "aduniverse.db")
+
+# display all table names in the database
+dbListTables(con)
+
+# Reads permits table and pull into a data frame
+dbReadTable(con, "permits")
+
+# Select in all entries in parcels db table
+dbGetQuery( con,sprintf("select %s from %s", "*", "permits") )
+
+# disconnect from the database
+dbDisconnect()
