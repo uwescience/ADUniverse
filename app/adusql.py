53c1a22188cfecd3059131ad1ae153c05af5a1a0
Adrian T <43564152+adrianmt18@users.noreply.github.com>
2019-07-05 11:38:34 -0700

    Update adusql.py
    Converted to utilize object-oriented functionality.

--- app/adusql.py
+++ app/adusql.py
@@ -2,55 +2,58 @@ import pandas as pd
 import sqlite3
 
 def hello():
-	print("Successfully imported aduSQL")
+    print("Successfully imported adusql")
 
-def dbConnect(dbName = 'aduniverse.db'):
-    '''
-    :string param dbName: the name of the database to connect to.
-    '''
-    return sqlite3.connect(dbName)
+class Connection:
+    def __init__(self, dbname = "aduniverse.db"):
+        '''
+        :string param dbname: the name of the database to connect to.
+        '''
+        self.dbname = dbname
+        self.conn = None
+        self.connected = False
 
-def createDB(df, con, tablename):
-    '''
-    This function connects to database DB and creates/modifies a database table tablename with values in df
-	df: dataframe to be converted into a database table
-	con = sqlite3.Connection object
-    tablename: the name of the new or existing database table to write to
-    Dependencies: sqlite3, pandas
-    
-    Example:
-    df = pd.read_csv("data.csv", sep=",",header=0)
-    conn = sqlite3.connect('mydata.db')
-    createDB(df, "new_table")
-    '''
-    df.to_sql(tablename, con, if_exists = 'replace', index=False)
-    return
+    def connect(self):
+        self.conn = sqlite3.connect(self.dbname)
+        self.connected = True
 
-def select(table, con, cols = "*"):
-    '''
-	table: database table to select table from
-	con = sqlite3.Connection object
-    cols (optional): list of columns to select from table. Default is * (all columns)
-    Dependencies: sqlite3, pandas
-    
-    Example:
-    conn = sqlite3.connect('mydata.db')
-    select("tablename", conn)
-    -OR-
-    select("tablename", conn, ['col1', 'col2', 'col3'])
-    '''
-    if type(cols) == list:
-        cols.reverse()
-        last = cols[0]
-        tempcols = ""
-        for col in range(0,len(cols)):
-            colname = cols.pop()
-            if colname == last:
-                tempcols = tempcols + colname
-            else:
-                tempcols = tempcols + colname + ", "
+    def createTable(self, df, tablename):
+        '''
+        This function creates/modifies a database table tablename with values in df
+        :pandas DataFarme param df: dataframe to be converted into a database table
+        :string param tablename: the name of the new or existing database table to write to
+        Dependencies: sqlite3, pandas
+        
+        Example:
+        df = pd.read_csv("data.csv", sep=",",header=0)
+        obj.createTable(df, "new_table")
+        Depend
+        '''
+        df.to_sql(tablename, self.conn, if_exists = 'replace', index=False)
+        
+    def select(self, tablename, cols = "*"):
+        '''
+        :string param table: database table to select table from
+        :list param cols (optional): list of columns to select from table. Default is * (all columns)
+        Dependencies: sqlite3, pandas
+        
+        Example:
+        obj.select("tablename")
+        -OR-
+        obj.select("tablename", ['col1', 'col2', 'col3'])
+        '''
+        if type(cols) == list:
+            cols.reverse()
+            last = cols[0]
+            tempcols = ""
+            for col in range(0,len(cols)):
+                colname = cols.pop()
+                if colname == last:
+                    tempcols = tempcols + colname
+                else:
+                    tempcols = tempcols + colname + ", "
                 
-        cols = tempcols
-    
-    SQLstring = "SELECT {} FROM {}".format(cols, table)
-    return pd.read_sql_query(SQLstring, con)
+            cols = tempcols
+        
+        SQLstring = "SELECT {} FROM {}".format(cols, tablename)
+        return pd.read_sql_query(SQLstring, self.conn)
