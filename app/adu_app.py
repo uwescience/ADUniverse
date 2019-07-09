f2483754788ba8121da53d08aabac54c8d2b6139
Niu Yuanhao <43457955+yniu87@users.noreply.github.com>
2019-07-08 17:30:49 -0700

    real_subsample

--- app/adu_app.py
+++ app/adu_app.py
@@ -1,5 +1,6 @@
 import folium
 import pandas as pd
+import adusql
 
 import json
 import sys
@@ -11,22 +12,21 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-FILE = "Seattle_Real_Time_Fire_911_Calls.csv"
+# FILE = "Seattle_Real_Time_Fire_911_Calls.csv"
+
+FILE = "adudata_UnivDist_small.csv"
 SEATTLE_COORDINATES = (47.6062, -122.3321)
 zoom_start = 12
 data = pd.read_csv(FILE)
 
 # for speed purposes
-MAX_RECORDS = 10
+MAX_RECORDS = 100
 
 # create empty map zoomed in on San Francisco
 map = folium.Map(location=SEATTLE_COORDINATES, zoom_start=zoom_start, control_scale=True)
 
 # add neighborhoods on top of this. This is an experiment to be replaced with a polygon geojson
 geo_json_data = json.load(open('neighborhoods.geojson'))
-parcel_data = json.load(open('Zoned_Parcels_forJSON.json'))
-# parcel2_data = json.load(open('Zoned_Parcels_forJSON_Featur.json'))
-
 # folium.GeoJson(geo_json_data).add_to(map)
 
 # regular style of polygons
@@ -55,14 +55,9 @@ def highlight_function(feature):
 
 # apply the neighborhood outlines to the map
 neighborhoods = folium.features.GeoJson(geo_json_data,
-                               style_function=style_function,
-                               highlight_function=highlight_function,
-                               )
-parcels = folium.features.GeoJson(parcel_data,
-                               style_function=style_function,
-                               highlight_function=highlight_function,
-                               )
-
+                                        style_function=style_function,
+                                        highlight_function=highlight_function,
+                                        )
 popup = folium.Popup('Hi')
 popup.add_to(neighborhoods)
 neighborhoods.add_to(map)
@@ -74,103 +69,37 @@ neighborhoodsearch = Search(
     collapsed=False,
     search_label='name',
     weight=3,
-    # fill_color='black'
-    kwargs={'fillColor': "black",
-             'fillOpacity': 0.6}
+    kwargs={'fillColor': "blue",
+            'fillOpacity': 0.6}
 ).add_to(map)
 # We need to fix kwargs and popups of polygons iterating through geojson
-# 
-
-def anaghaTest(neighborhoodsearch,**kwargs):
-    for key, value in kwargs.items():
-        print("%s==%s"%(key,value))
-
-#driver code
-anaghaTest(neighborhoodsearch, fill_color ='black', fill_opacity =0.6)
+#
 
 
 # print(geo_json_data[1,:])
-geo_json_data_df = pd.DataFrame.from_dict(geo_json_data)
-geo_json_data_df.to_csv(r'/Users/Anaavu/Documents/GitHub/ADUniverse/app/geo_json_data_df.csv')
+#geo_json_data_df = pd.DataFrame.from_dict(geo_json_data)
+# geo_json_data_df.to_csv(r'/Users/Anaavu/Documents/GitHub/ADUniverse/app/geo_json_data_df.csv')
 # Anagha
 
 
 # add a marker for every record in the filtered data, use a clustered view
 for _, row in data[0:MAX_RECORDS].iterrows():
-    popup = folium.Popup("Feasibility: " + str(row['Random Number']) +
-                         "<br> Address: " + str(row['Address']), max_width=300)
+    popup = folium.Popup("Year Build: " + str(row['YRBUILT']) +
+                         "<br> Address: " + str(row['ADDRESS']), max_width=300)
     # html_str = """
     # <a href="https://www.ibm.com/" target="_blank"> Details.</a>
     # """
     # iframe = folium.IFrame(html=html_str, width=100, height=50)
     # popup = folium.Popup(iframe, max_width=2650)
-    folium.Marker([row['Latitude'], row['Longitude']], popup=popup).add_to(map)
-
-
-# for _, row in geo_json_data['features']:
-#     for i in row['properties']:
-#         popup = folium.Popup(i['name'])
-#     # c.add_to(map)
+    folium.Marker([row['INTPTLA'], row['INTPTLO']], popup=popup).add_to(map)
 
 map.save("map.html")
+
 # Dashify
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash("SeattleADU",
                 external_stylesheets=external_stylesheets)
-
-# app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
-
-# app.layout = html.Div([
-#     html.H1("Seattle ADU Feasibility"),
-#     html.Iframe(id='map', srcDoc=open("map.html", "r").read(),
-#                 width="90%", height="400"),
-#     html.H3("Lot Size"),
-#     dcc.Slider(
-#         id='my-range-slider',
-#         min=1000,
-#         max=20000,
-#         step=100,
-#         value=4000
-#     ),
-#     html.Div(id='output-container-range-slider'),
-#     # html.Iframe(id="parent", srcDoc=html_frames, width="100%",
-#     #    height="600"),
-#     # html.Iframe(id='map', srcDoc=open("map.html", "r").read(),
-#     #    width="100%", height="600")
-# ])
-
-# Dropdown for given cities
-# app.layout = html.Div([
-#     html.H1("Seattle ADU Feasibility"),
-#     html.Iframe(id='map', srcDoc=open("map.html", "r").read(),
-#                 width="90%", height="400"),
-#     html.H3("Find your home"),
-#     dcc.Dropdown(
-#         id='my-dropdown',
-#         options=[
-#             {'label': 'New York City', 'value': 'NYC'},
-#             {'label': 'Montreal', 'value': 'MTL'},
-#             {'label': 'San Francisco', 'value': 'SF'}
-#         ],
-#         value='SF'
-#     ),
-#     html.Div(id='output-container')
-# ])
-
-# Dropdown base on the dataframe
-
-
-# def generate_table(data, MAX_RECORDS):
-#     return html.Table(
-#         # Header
-#         [html.Tr([html.Th(col) for col in data.Address])] +
-#         # Body
-#         [html.Tr([
-#             html.Td(data.iloc[i][col]) for col in data.Address
-#         ]) for i in range(min(len(data), MAX_RECORDS))]
-#     )
-
 
 app.layout = html.Div([
     html.H1("Seattle ADU Feasibility"),
@@ -178,10 +107,10 @@ app.layout = html.Div([
                 width="100%", height="550"),
     html.H3("Find your home"),
     dcc.Dropdown(
-        id='my-dropdown',
+        id='addressDropdown',
 
         options=[
-            {'label': i, 'value': i} for i in data.Address.unique()
+            {'label': i, 'value': i} for i in data.ADDRESS.unique()
         ],
         placeholder='Filter by address...'),
 
@@ -190,13 +119,25 @@ app.layout = html.Div([
 
     html.Div(id='output-container'),
 
+    html.H2("Why you want an ADU?"),
+    dcc.Dropdown(
+        id='my-dropdown',
+        options=[
+            {'label': 'Build one more unit for rental income', 'value': 'income'},
+            {'label': 'A Relative needs some housing', 'value': 'support'},
+        ],
+        value='purposes'
+    ),
+    html.Div(id='output_drop'),
+
     html.H3("tell me your lot size"),
     dcc.Input(id='my-id', value='initial value', type='number'),  # text
     # html.Div(id='my-div'),
     html.Table([
         html.Tr([html.Td(['lot_size']), html.Td(id='my-div')]),
         # html.Tr([html.Td(['intermediate']), html.Td(id='intermediate-value')]),
-        html.Tr([html.Td(['ADU score']), html.Td(id='calculation')]),
+        html.Tr([html.Td(['ADU score']), html.Td(id='calculation')])
+
     ]),
 
     dcc.Markdown('''
@@ -221,12 +162,12 @@ app.layout = html.Div([
 
 @app.callback(
     dash.dependencies.Output('intermediate-value', 'children'),
-    [dash.dependencies.Input('my-dropdown', 'value')]
+    [dash.dependencies.Input('addressDropdown', 'value')]
 )
 def clean_data(value):
     # some expensive clean data step
-    dff = data.loc[data['Address'] == value].head(1)
-    cleaned_df = dff["Random Number"]
+    dff = data.loc[data['ADDRESS'] == value].head(1)
+    cleaned_df = dff["YRBUILT"]
 
     # more generally, this line would be
     # json.dumps(cleaned_df)
@@ -237,24 +178,14 @@ def clean_data(value):
 
 
 @app.callback(
-    # dash.dependencies.Output('output-container-range-slider', 'children'),
     dash.dependencies.Output('output-container', 'children'),
-    [dash.dependencies.Input('my-dropdown', 'value')]
+    [dash.dependencies.Input('addressDropdown', 'value')]
 )
-# [dash.dependencies.Input('my-range-slider', 'value')])
 def update_output(value):
     # dff = data[data.Address.str.contains('|'.join(value))]
-    dff = data.loc[data['Address'] == value].head(1)
-    dff = dff["Random Number"].values
+    dff = data.loc[data['ADDRESS'] == value].head(1)
+    dff = dff["YRBUILT"].values
     return 'You have selected_random number "{}"'.format(dff)
-# def display_table(dropdown_value):
-#     # import pdb
-#     # pdb.set_trace()
-#     if dropdown_value is None:
-#         return generate_table(data, MAX_RECORDS)
-#
-#     dff = data[data.Address.str.contains('|'.join(dropdown_value))]
-#     return generate_table(dff, MAX_RECORDS)
 
 
 @app.callback(
@@ -262,13 +193,22 @@ def update_output(value):
      Output(component_id='calculation', component_property='children')],
     [Input(component_id='my-id', component_property='value'),
      Input(component_id='intermediate-value', component_property='children')]
+    # Input(component_id='output_drop', component_property='children')]
 )
 def update_output_div(input_value, json):  # orient='index'
+    # aux = pd.read_json(json, orient='values').values
     aux = pd.read_json(json, orient='values').values[0][1]
     # aux = pd.read_json(json, orient='values').iloc[0].values
     # aux = pd.read_json(json, orient='values')
     return 'You\'ve entered "{}" for lot size'.format(input_value), aux*input_value
 
+
+@app.callback(
+    dash.dependencies.Output('output_drop', 'children'),
+    [dash.dependencies.Input('my-dropdown', 'value')])
+def update_output(value):
+    return 'You want "{}"'.format(value)
+    # return cleaned_df.to_json(orient='split')
 
 # app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
@@ -278,7 +218,6 @@ def update_output_div(input_value, json):  # orient='index'
 #                Input('input-2-keypress', 'value')])
 # def update_output(input1, input2):
 #     return u'Input 1 is "{}" and Input 2 is "{}"'.format(input1, input2)
-
 
 
 if __name__ == '__main__':
