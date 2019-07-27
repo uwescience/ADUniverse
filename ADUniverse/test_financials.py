c2cc9086208bb7329106c9af77576ad4d568daf2
Adrian T <43564152+adrianmt18@users.noreply.github.com>
2019-07-26 17:56:38 -0700

    Update test_financials.py

--- ADUniverse/test_financials.py
+++ ADUniverse/test_financials.py
@@ -1,4 +1,4 @@
-import financials
+import financials as fin
 
 # Structural Parameter Assumptions
 APR = 0.069  # anual interest rate
@@ -8,14 +8,17 @@ MATURITY = 15  # loan MATURITY years
 def test_loan_calculator():
     loan = 150000
     feature = 3
-    output1, output2 = financials.loan_calculator(loan, feature)
-    assert (float(output1) != 0)
-    assert (float(output2) != 0)
+    output1, output2 = fin.loan_calculator(loan, feature)
+    assert(isinstance(output1, str))
+    assert(isinstance(output2, str))
 
 
 def test_cost_breakdown():
-    construction, sewer, design, total = financials.cost_breakdown(True, 500)
-    assert(isinstance(float(construction), float))
-    assert(isinstance(float(sewer), float))
-    assert(isinstance(float(design), float))
-    assert(isinstance(float(total), float))
+    construction, tax, sewer, permit, design, total, p_tax = fin.cost_breakdown(True, 500)
+    assert(isinstance(construction, str))
+    assert(isinstance(tax, str))
+    assert(isinstance(sewer, str))
+    assert(isinstance(permit, str))
+    assert(isinstance(design, str))
+    assert(isinstance(total, str))
+    assert(isinstance(p_tax, str))
