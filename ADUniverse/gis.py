36dbefbd374e9395246de6537204dbb14dcc195a
Anagha Uppal <anaavu@gmail.com>
2019-07-30 09:58:16 -0700

    Create gis.py

---
+++ ADUniverse/gis.py
@@ -0,0 +1,10 @@
+import fiona
+
+lines = fiona.open("/Users/Anaavu/Desktop/Anagha/DSSG/data/parcel_poly2line/parcel_poly2line.shp")
+
+# first = lines.next(iter(collection))
+# print(first)
+
+# for each fid in parcel_fid
+
+# print first['type']
